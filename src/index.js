69469e4635b4c7945a1abb24ce593c17887e25ee
Chris Dufour <chris@cdufour.com>
2019-05-09 00:08:08 -0400

    Added some stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 function component() {
 	const element = document.createElement("div");
-
-	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = "Hello, World!";
+	element.innerHTML = "Hello my dude!";
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-	entry: "./src/js/index.js",
+	mode: "development",
+	entry: path.join(__dirname, "src", "index.js"),
 	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist")
+		path: path.join(__dirname, "/dist"),
+		publicPath: "/",
+		filename: "index_bundle.js",
+		chunkFilename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /.jsx?$/,
+				include: [path.resolve(__dirname, "src")],
+				exclude: [path.resolve(__dirname, "node_modules")],
+				loader: "babel-loader",
+				query: {
+					presets: [
+						[
+							"@babel/env",
+							{
+								targets: {
+									browsers: "last 2 chrome versions"
+								}
+							}
+						]
+					]
+				}
+			}
+		]
+	},
+
+	plugins: [new HtmlWebpackPlugin({})],
+	resolve: {
+		extensions: [".json", ".js", ".jsx"]
+	},
+	devtool: "source-map",
+	devServer: {
+		contentBase: path.join("/dist/"),
+		inline: true,
+		host: "0.0.0.0",
+		port: 8080
 	}
 };
